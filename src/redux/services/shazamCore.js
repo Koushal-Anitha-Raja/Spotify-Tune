e2ad88b76702a928d602a91336767014096507dd
Ar Koushal <arkoushal1999@gmail.com>
2024-01-25 12:07:15 -0600

    API connection and worked on Discover component

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "fabf70dad5mshbc11a9b5a369599p18acdfjsne631d0041f2e",
-    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
-  },
-};
-
-fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
-
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
+  baseurl: fetchBaseQuery({
+    baseurl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "fabf70dad5mshbc11a9b5a369599p18acdfjsne631d0041f2e"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: "/charts/world" }),
+  }),
 });
+
+//redux allows to use the getTopCharts function to export as a hook
+export const { useGetTopChartsQuery } = shazamCoreApi;
