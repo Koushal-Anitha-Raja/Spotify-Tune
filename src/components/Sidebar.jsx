f51747cb905b5597dba9571c4500673111372207
Ar Koushal <arkoushal1999@gmail.com>
2024-02-02 20:35:02 -0600

    fixed sidebar icon bug

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,14 +5,14 @@ import { logo } from "../assets";
 import { links } from "../assets/constants";
 import { HiOutlineMenu } from "react-icons/hi";
 
-const NavLinks = ({}) => (
+const NavLinks = ({ handleClick }) => (
   <div className="mt-10">
     {links.map((item) => (
       <NavLink
         className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
         key={item.name}
         to={item.to}
-        onClick={() => handleclick}
+        onClick={() => handleClick && handleClick()}
       >
         <item.icon className="w-6 h-6 mr-2" />
         {item.name}
@@ -31,10 +31,24 @@ const Sidebar = () => {
       </div>
       <div className="absolute md:hidden block top-6 right-3">
         {mobileMenuOpen ? (
-          <RiCloseLine className="w-6 h-6 text-white mr-2" />
+          <RiCloseLine
+            className="w-6 h-6 text-white mr-2"
+            onClick={() => setmobileMenuOpen(false)}
+          />
         ) : (
-          <HiOutlineMenu className="w-6 h-6 text-white mr-2" />
+          <HiOutlineMenu
+            className="w-6 h-6 text-white mr-2"
+            onClick={() => setmobileMenuOpen(true)}
+          />
         )}
+      </div>
+      <div
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-t from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+          mobileMenuOpen ? "left-0" : "-left-full"
+        }`}
+      >
+        <img src={logo} alt="logo" className="w-full h-14 object-contain"></img>
+        <NavLinks handleClick={() => setmobileMenuOpen(false)} />
       </div>
     </>
   );
